3eec4534f9e51b7c7fca7c4646ef389b45f5182a
Louis <louis.dhordain065@gmail.com>
2026-09-08 14:26:53 +0200

    Ajoutez des fichiers projet.

---
+++ test/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int V = 0; 
+
+
+    if (V >= 0) {
+        
+
+        printf("%d\n", V);
+    }
+    else {
+       
+
+        printf("%d\n", -V);
+    }
+
+    return 0;
+}
